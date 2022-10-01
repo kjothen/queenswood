5987c0ba574e3909e548c5297d6d2d895f0cd5a6
kjothen <kieran.othen@mac.com>
2022-10-01 21:43:08 +0100

    Trim local

--- development/src/dev/local.clj
+++ development/src/dev/local.clj
@@ -1,34 +1,9 @@
 (ns dev.local)
 
-(comment)
-
 (comment
   ;; open a debug portal
   (require '[portal.api :as p])
-  (def p (p/open {:launcher :vs-code}))
+  (def p (p/open))
   (add-tap #'p/submit) ; Add portal as a tap> target
   (tap> "Opened tap")
   (p/close))
-
-(comment
-  ;; experiment with pulsar
-  (require '[com.repldriven.mono.pulsar.interface :as pulsar]
-           '[com.repldriven.mono.env.interface :as env]
-           '[clojure.java.io :as io]
-           '[donut.system :as ds])
-
-  (env/set-env! (io/resource "pulsar/test-env.edn") :default)
-  (def system-config (pulsar/configure-system (get-in @env/env [:system :pulsar])))
-
-  (tap> system-config)
-
-  (tap> booted-system)
-  (def booted-system (ds/start system-config nil #{[:pulsar :admin] [:pulsar :service-url]}))
-
-  (def admin (get-in booted-system [::ds/instances :pulsar :admin]))
-  (pulsar/ensure-topic admin "persistent://tenant/namespace/identity")
-
-  (def system (ds/start booted-system))
-
-  (ds/stop system)
-)
