1d1f8f9f4dc468224c77b37001a26c925d0e09a1
kjothen <kieran.othen@mac.com>
2022-12-05 09:24:49 +0000

    tweak route

--- bases/blocking-command-api/src/com/repldriven/mono/blocking_command_api/api.clj
+++ bases/blocking-command-api/src/com/repldriven/mono/blocking_command_api/api.clj
@@ -20,25 +20,28 @@
 (def CommandResult [:map [:result string?]])
 (def CommandResponse (ResponseData CommandResult))
 
+(def routes
+  ["/api"
+   ["/command"
+    {:post {:summary "negotiated request & response (json, edn, transit)"
+            :parameters {:body CommandRequest}
+            :responses {200 {:body CommandResponse}}
+            :handler (fn [{{{{:keys [type id]} :data} :body} :parameters}]
+                       {:status 200
+                        :body {:data {:result (string/join "/"
+                                                           [type id])}}})}}]])
+
 (defn app
   [{:keys [pulsar-client mqtt-client]}]
   (ring/ring-handler
-   (ring/router
-    ["/api"
-     ["/command"
-      {:post {:summary "negotiated request & response (json, edn, transit)"
-              :parameters {:body CommandRequest}
-              :responses {200 {:body CommandResponse}}
-              :handler (fn [{{{{:keys [type id]} :data} :body} :parameters}]
-                         {:status 200
-                          :body {:data {:result (string/join "/"
-                                                             [type id])}}})}}]]
-    ;; router data affecting all routes
-    {:data {:muuntaja m/instance
-            :coercion malli/coercion
-            :exception pretty/exception
-            :middleware
-            [muuntaja/format-middleware rrc/coerce-exceptions-middleware
-             rrc/coerce-request-middleware rrc/coerce-response-middleware]}})))
+   (ring/router routes
+                ;; router data affecting all routes
+                {:data {:muuntaja m/instance
+                        :coercion malli/coercion
+                        :exception pretty/exception
+                        :middleware [muuntaja/format-middleware
+                                     rrc/coerce-exceptions-middleware
+                                     rrc/coerce-request-middleware
+                                     rrc/coerce-response-middleware]}})))
 
 (comment)
