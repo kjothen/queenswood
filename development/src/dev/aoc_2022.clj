d532594d47e7b1751e2a7aee7aecf9959f3d42e8
kjothen <kieran.othen@mac.com>
2022-12-08 15:26:31 +0000

    nearly part 2 of day8

--- development/src/dev/aoc_2022.clj
+++ development/src/dev/aoc_2022.clj
@@ -411,60 +411,72 @@ $ ls
   (parse (io/resource "aoc-2022/07/input.dat")))
 
 (comment
+  (require '[clojure.java.io :as io]
+           '[clojure.string :as string]
+           '[com.rpl.specter :refer [ALL transform]])
+  (defn parse-uint [s] (Integer/parseUnsignedInt s))
+  (defn cell [vs m n] (nth (nth vs m) n))
+  (defn make-range
+    [vs m n view]
+    (case view
+      :up (reverse (range 0 m))
+      :left (reverse (range 0 n))
+      :right (range (inc n) (count vs))
+      :down (range (inc m) (count vs))))
+  (defn make-coll
+    [vs m n view]
+    (case view
+      (:up :down) (reduce (fn [res m'] (conj res (cell vs m' n)))
+                          []
+                          (make-range vs m n view))
+      (:left :right) (reduce (fn [res n'] (conj res (cell vs m n')))
+                             []
+                             (make-range vs m n view))))
+  (defn view-visible?
+    [vs m n view]
+    (let [v (cell vs m n)]
+      (reduce (fn [res v'] (if (<= v v') (reduced false) res))
+              true
+              (make-coll vs m n view))))
+  (defn view-score
+    [vs m n view]
+    (let [v (cell vs m n)
+          coll (make-coll vs m n view)
+          asc (map (fn [[x y]] (and (<= x y) (> v x))) (partition 2 1 coll))]
+      (if (> v (first coll))
+        (reduce (fn [res v'] (if (false? v') res (inc res))) 1 asc)
+        1)))
+  (defn cell-visible?
+    [vs m n]
+    (or (view-visible? vs m n :up)
+        (view-visible? vs m n :left)
+        (view-visible? vs m n :right)
+        (view-visible? vs m n :down)))
+  (defn visibles
+    [vs]
+    (for [m (range 1 (dec (count vs)))
+          n (range 1 (dec (count vs)))
+          :when (cell-visible? vs m n)]
+      (cell vs m n)))
+  (defn cell-score
+    [vs m n]
+    [(view-score vs m n :up) (view-score vs m n :left) (view-score vs m n :down)
+     (view-score vs m n :right)])
+  (defn scores
+    [vs]
+    (for [m (range 1 (dec (count vs)))
+          n (range 1 (dec (count vs)))]
+      (cell-score vs m n)))
   (def test-data "30373
 25512
 65332
 33549
 35390")
+  (def input-data (slurp (io/resource "aoc-2022/08/input.dat")))
   (def vs
-    (->> (string/split-lines test-data)
+    (->> (string/split-lines input-data)
          (map (partial partition 1))
          (mapv (comp vec flatten))
          (transform [ALL ALL] #(parse-uint (str %)))))
-  (defn lookup
-    [vs m n]
-    (prn "lookup" m n)
-    (-> vs
-        (nth m)
-        (nth n)))
-  (defn bottom-visible?
-    [vs m n]
-    (let [v (lookup vs m n)
-          res (for [m' (range (inc m) (count vs))
-                    :let [v' (lookup vs m' n)]
-                    :while (> v v')]
-                v)]
-      (not (empty? res))))
-  (defn right-visible?
-    [vs m n]
-    (let [v (lookup vs m n)]
-      (not (empty? (for [n' (range n (count vs))
-                         :let [v' (lookup vs m n')]
-                         :while (< v v')]
-                     v)))))
-  (defn top-visible?
-    [vs m n]
-    (let [v (lookup vs m n)]
-      (not (empty? (for [m' (reverse (range 0 m))
-                         :let [v' (lookup vs m' n)]
-                         :while (< v v')]
-                     v)))))
-  (defn left-visible?
-    [vs m n]
-    (let [v (lookup vs m n)]
-      (not (empty? (for [n' (reverse (range 0 n))
-                         :let [v' (lookup vs m n')]
-                         :while (< v v')]
-                     v)))))
-  (defn visible?
-    [vs m n]
-    (bottom-visible? vs m n)
-    ;(and (bottom-visible? vs m n)
-    ;     (right-visible? vs m n)
-    ;     (top-visible? vs m n)
-    ;     (left-visible? vs m n))
-  )
-  (for [m (range 1 (dec (count vs)))
-        n (range 1 (dec (count vs)))
-        :when (visible? vs m n)]
-    (lookup vs m n)))
+  (+ (count (visibles vs)) (- (* 4 (count vs)) 4))
+  (apply max (mapv (partial apply *) (scores vs))))
